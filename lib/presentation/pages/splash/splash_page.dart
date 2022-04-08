3ee89eba65330da0054f52d9b6a7974c58827455
Jeremia Christian Aditya Perdana <jejechristianap@gmail.com>
2022-04-08 15:13:02 +0700

    Refactor package import

--- lib/presentation/pages/splash/splash_page.dart
+++ lib/presentation/pages/splash/splash_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_teleheal/config/styles/app_assets.dart';
 import 'package:flutter_teleheal/presentation/controller/splash/splash_controller.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
 
 class SplashPage extends GetView<SplashController> {
   const SplashPage({Key? key}) : super(key: key);
