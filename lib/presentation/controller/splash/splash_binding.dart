b74c72bc90361c65aae44b9d657ae7096c683566
Jeremia Christian Aditya Perdana <jejechristianap@gmail.com>
2022-04-08 10:58:26 +0700

    Bind splash controller

---
+++ lib/presentation/controller/splash/splash_binding.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_teleheal/presentation/controller/splash/splash_controller.dart';
+import 'package:get/instance_manager.dart';
+
+class SplashBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<SplashController>(() => SplashController());
+  }
+}
