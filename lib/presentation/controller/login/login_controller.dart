02a9bf9240c9b1023737ab0d7902cec595ed1d6e
Jeremia Christian Aditya Perdana <jejechristianap@gmail.com>
2022-04-09 21:40:47 +0700

    Validate form

--- lib/presentation/controller/login/login_controller.dart
+++ lib/presentation/controller/login/login_controller.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
+
+import 'package:flutter_teleheal/config/route/route_constant.dart';
 import 'package:get/get.dart';
 import 'package:reactive_forms/reactive_forms.dart';
+
+import '../../widgets/progress_dialog.dart';
 
 class LoginController extends GetxController {
   final form = fb.group({
@@ -19,4 +24,23 @@ class LoginController extends GetxController {
   String get phoneNumber => form.control('phone_number').value;
 
   String get email => form.control('email').value;
+
+  bool get isValid {
+    if (form.invalid) {
+      form.focus(form.errors.entries.first.key);
+      form.unfocus();
+    }
+    return form.valid;
+  }
+
+  login() async {
+    Get.dialog(
+      const ProgressDialog(),
+      barrierDismissible: true,
+    );
+    log('dialog: ${Get.isDialogOpen}');
+    await 2.delay();
+    Get.back();
+    Get.offNamed(RouteConstant.main);
+  }
 }
