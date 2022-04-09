b3a9818ea7757d4cd014b421c05b5cc9e7aa752b
Jeremia Christian Aditya Perdana <jejechristianap@gmail.com>
2022-04-09 17:34:35 +0700

    Fix root app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_teleheal/presentation/teleheal_app.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const TelehealApp(),
-    );
-  }
-}
+void main() => runApp(const TelehealApp());
