c71900d4cc54aa1fa2306bc263c6ea2209508225
Jeremia Christian Aditya Perdana <jejechristianap@gmail.com>
2022-04-09 17:35:19 +0700

    Define HomeBinding

---
+++ lib/presentation/controller/home/home_binding.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_teleheal/presentation/controller/home/home_controller.dart';
+import 'package:get/instance_manager.dart';
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(() => HomeController());
+  }
+}
